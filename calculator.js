d81e73d27afea2dcf18b664979ca8e8ce41d2b83
Sean Nakamura <seanenakamura@gmail.com>
2015-05-02 14:54:57 -1000

    betterway

--- calculator.js
+++ calculator.js
@@ -6,6 +6,23 @@
  * @return {object} `calculator` object that can be used
  */
 
+function calculatorModule(){
+  var memory = 0;
+  var total = 0;
+
+  var calculator = {
+    load: validate(_load),
+    getTotal: _getTotal,
+    add: validate(_add),
+    subtract: validate(_subtract),
+    multiply: validate(_multiply),
+    divide: validate(_divide),
+    recall: _recall,
+    save: _save,
+    clear: _clear,
+  };
+
+  return calculator;
 
 
   /**
@@ -14,11 +31,30 @@
    * @return { Number }    current total
    */
 
+   function _load(x){
+    // if (typeof x != "number"){
+    //   return "error";
+    // }
+    total = x;
+    return total;
+   }
 
+   function validate(fn){
+    return function (x){
+      if(typeof x != "number"){
+        return "error";
+      }
+      return fn(x);
+    };
+   }
   /**
    * Return the value of `total`
    * @return { Number }
    */
+
+   function _getTotal(){
+    return total;
+   }
 
 
   /**
@@ -26,11 +62,27 @@
    * @param { Number } x
    */
 
+   function _add(x){
+    // if (typeof x != "number"){
+    // return "error";
+    // }
+    total += x;
+    return total;
+   }
+
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+
+   function _subtract(x){
+    // if (typeof x != "number"){
+    // return "error";
+    // }
+    total -= x;
+    return total;
+   }
 
 
   /**
@@ -38,11 +90,27 @@
    * @param  { Number } x
    */
 
+   function _multiply(x){
+    // if (typeof x != "number"){
+    // return "error";
+    // }
+    total *= x;
+    return total;
+   }
+
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
+
+   function _divide(x){
+    // if (typeof x != "number"){
+    // return "error";
+    // }
+    total /= x;
+    return total;
+   }
 
 
   /**
@@ -50,13 +118,28 @@
    */
 
 
+   function _save(){
+    memory = total;
+    return memory;
+   }
+
+
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
+
+   function _recall(){
+    return memory;
+   }
 
 
   /**
    * Clear the value stored at `memory`
    */
 
+   function _clear(){
+    memory = 0;
+    return memory;
+   }
+}
